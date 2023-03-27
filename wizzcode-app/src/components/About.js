010b74d7ca617d16c5c60df8303e04b97cd29945
Samiksha Shrimali <59738539+samiksha024@users.noreply.github.com>
2023-03-27 23:13:43 +0530

    initial react frontend

---
+++ wizzcode-app/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+const About = () => {
+  return (
+    <div>     
+      <div style={{backgroundColor:"purple"}}><h2 style={{ fontSize: 35,color:"white", textAlign: "left", padding:'20px'}}>About The Team</h2><p style={{ fontSize: 20,color:"white", textAlign: "left", padding:'50px'}}>Isha Dhabalia<br></br>Mahi Dedhia<br></br>Aditi Paretkar<br></br>Samiksha Shrimali</p></div>
+    </div>
+  );
+};
+  
+export default About;
