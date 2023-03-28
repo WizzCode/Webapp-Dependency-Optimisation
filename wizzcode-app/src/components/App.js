aaefbe1ff75d116d51885bdacd48646bc348360b
ishadhabalia <isha.dhabalia@somaiya.edu>
2023-03-29 02:03:14 +0530

    Header and Banner frontend
    Co-authored-by: Samiksha <s.shrimali@somaiya.edu>

--- wizzcode-app/src/components/App.js
+++ wizzcode-app/src/components/App.js
@@ -1,44 +1,22 @@
 import React from "react";
 import {Router, Switch, Routes, Route} from "react-router-dom";
 import './App.css';
-import Navbar from "./Navbar";
 import Header from "./Header"; 
-import Button from "./Button";
 import FileUploadButton from "./FileUploadButton";
+import { Link } from "react-router-dom";
 
-import About from "./About";
-import Description from "./Description";
+import { Banner } from "./Banner";
+
+
 function App() {
-  /*let component
-  switch (window.location.pathname)
-{
   
-  case "/description":
-    component =<Description />
-  break
-    case "/about":
-    component =<About />
-  break}*/
-
- return (
+return (
     
-    <div id="container" >
-      
-     <style>{'body { background-color: blue; display:"flex"}' }
-      
-     </style>
-     
-    <Navbar/> 
-    <Header /> 
-    <div style={{backgroundColor:"black"}}><h2 style={{ fontSize: 30,color:"white",textAlign: "center",padding:'20px'}}>Dependency Relationship</h2><p style={{ color:"white", fontSize: 20, textAlign: "center", padding:'50px'}}>Dependency relations help us to understand the logic behind how variables and other attributes in a code are related to one another. This thus, helps us to understand the code better, make correct changes and helps in software reusabiity as well.</p></div>
-    <div style={{backgroundColor:"black"}}><h2 style={{ fontSize: 30, color:"white",textAlign: "center", padding:'20px'}}>Code Optimization</h2><p style={{ color:"white",fontSize: 20, textAlign: "center", padding:'50px'}}>Code optimization is a program modification strategy that endeavours to enhance the intermediate code, so a program utilises the least potential memory, minimises its CPU time and offers high speed.</p></div>
-
-    <FileUploadButton/>
-    <Button/> 
-    
-    <Description />
-    <About />
-    </div>
+  <main className="text-gray-400 bg-gray-900 body-font">
+  <Header />
+  <Banner />
+  <FileUploadButton/>
+</main>
 
 
      );}
--- wizzcode-app/src/components/Header.js
+++ wizzcode-app/src/components/Header.js
@@ -1,12 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
-const Header=() => {
-    return (
-    <div className="ui fixed menu">
-      <div className="ui container center" ></div>  
-     
-      <h2 style={{ fontSize: 60, textAlign: "center" }}>WizzCode</h2>
-    </div>
-    );
-};
-export default Header;
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+
+export const Header = () => {
+
+  return (
+ 
+      <Navbar expand="md" >
+        <Container>
+          <Navbar.Brand href="/">
+            <h1>WizzCode</h1>
+          </Navbar.Brand>
+
+        </Container>
+      </Navbar>
+   
+  )
+}
+
+export default Header
