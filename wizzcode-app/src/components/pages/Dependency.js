ee8fd1a09310648f11fe7ee52ceb3634e9ada3d2
alwaysaditi <68003668+alwaysaditi@users.noreply.github.com>
2023-04-08 17:29:48 +0530

    Added color scheme for variables according to type in react-graph

--- wizzcode-app/src/components/pages/Dependency.js
+++ wizzcode-app/src/components/pages/Dependency.js
@@ -7,14 +7,29 @@ import Graph from 'react-graph-vis';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 function Dependency() { 
-  const names = ["a","b","c"];
+  const names = ["a","b","c","d","e","f"];
+  const types = [1,2,1,1,3,7];
+  const nodeTypes = {
+    1: '#FF0000', // red
+    2: '#00FF00', // green
+    3: '#0000FF',
+    4:'#FFFF00',
+    5:'#800080',
+    6:'#FFA500',
+    7:'#FFC0CB',
+    8: '#40E0D0',
+    9: '#E6E6FA',
+    10: '#FF00FF'// blue
+    // blue
+  };
+  
   const links = [[true,false,true],
   [false, false, true],
   [true,false,false]];
   const nodesGraph =[];
   const edgesGraph =[];
   for (let i = 0; i<names.length; i++) {
-    nodesGraph.push({id: i, label: `${names[i]}`}); // id is just the integer which will be used to
+    nodesGraph.push({id: i, label: `${names[i]}`, color: nodeTypes[types[i]]}); // id is just the integer which will be used to
     // identify the nodes and will not be displayed on the graph
   }
   for(let i=0;i<links.length;i++)
@@ -27,6 +42,7 @@ function Dependency() {
   }
   }
   }
+
     const [graph, setGraph] = useState({
       nodes: nodesGraph,
       edges: edgesGraph
@@ -39,6 +55,7 @@ function Dependency() {
       edges: {
         color: 'white'
       }
+     
     };
   
     const events = {
