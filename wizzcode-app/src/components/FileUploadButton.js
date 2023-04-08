39ef75b1ba70682fd9dcfa973918eaf1df7b8158
Samiksha Shrimali <59738539+samiksha024@users.noreply.github.com>
2023-04-08 17:12:37 +0530

    added pop ups using modal to display upload successful or not

--- wizzcode-app/src/components/FileUploadButton.js
+++ wizzcode-app/src/components/FileUploadButton.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import  { useState } from 'react';
+import {useState} from 'react';
 import { Context } from './ContextFile';
-
+import { Modal, Button } from 'react-bootstrap';
 class FileUploadButton extends Component {
 
     static contextType = Context;
@@ -10,21 +10,31 @@ class FileUploadButton extends Component {
     // API Endpoints
     custom_file_upload_url = `http://127.0.0.1:8080/api/optimisation`;
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: null,
-            fileText: null,
-        }
+    //constructor(props) {
+        //super(props);
+        //this.state = {
+            //file: null,
+           //fileText: null,
+        
+    
+    state = {
+        file: null,
+        fileText: null,
+        success: false, // define success as a class property
+        showModal: false,
     }
+        
 
-   
     handleFile = (e) => {
+        
+        
         let files = e.target.files[0];
 
         this.setState({
             file: files,
+            success: false,
         })
+        
     }
 
     resolveTextFromFile = async (file) => {
@@ -38,20 +48,30 @@ class FileUploadButton extends Component {
             };
         });
     }
-   
+    handleCloseModal = () => {
+        this.setState({ showModal: false });
+    }
     handleSubmitFile = () => {
         
+        if (this.state.file === null) {
+            this.setState({
+                showModal: true,
+                success: false
+            });
+            return;
+        }
 
-        if (this.state.file !== null){
-            let formData = new FormData();
-            formData.append('codeInput', this.state.file);
+        let formData = new FormData();
+        formData.append('codeInput', this.state.file);
 
-            let promiseObj = this.resolveTextFromFile(this.state.file);
-            promiseObj.then((value) => {
-                this.setState({
-                    fileText:value,
-                });
-            })
+        let promiseObj = this.resolveTextFromFile(this.state.file);
+        promiseObj.then((value) => {
+            this.setState({
+                fileText:value,
+            });
+        })
+
+        
             
             axios.post(
                 this.custom_file_upload_url,
@@ -68,19 +88,27 @@ class FileUploadButton extends Component {
                 const result = JSON.parse(JSON.stringify(res.data));
                 this.context.setInputFileText(this.state.fileText);
                 this.context.setOptimisationResponse(result);
+                this.setState({
+                    success: true,
+                    showModal: true,
+                });
             })
             .catch(err => {
                 console.log(err);
+                this.setState({
+                    showModal: true,
+                });
             })
+        
         }
-    }
+    
 
     
     // render from here
     render() { 
         return (
             <div>
-               
+            
                 {/*input field */}
                 <input class="choosefile"
                     type="file"
@@ -88,10 +116,23 @@ class FileUploadButton extends Component {
                 />
                 {/* <label>Upload file</label> */}
                 <input class="uploadbutton" type="submit" onClick={this.handleSubmitFile} value="Upload"/>
-                
+                {this.state.success ? <p style={{ color: "blue" }}>File uploaded successfully!</p> : null}
+                <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+                <Modal.Header closeButton>
+                    {this.state.success ? <Modal.Title style={{ color: "black" }}>Success!</Modal.Title> : <Modal.Title style={{ color: "black" }}>Error!</Modal.Title>}
+                </Modal.Header>
+                <Modal.Body style={{ color: "black" }}>
+                    {this.state.success ? <p>Your file has been uploaded successfully.</p> : <p>{this.state.file === null ? 'Please select a file to upload.' : 'There was an error uploading your file. Please try again later.'}</p>}
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleCloseModal}>
+                    Close
+                </Button>
+            </Modal.Footer>
+           </Modal>
+
             </div>
         );
     }
 }
-
 export default FileUploadButton;
