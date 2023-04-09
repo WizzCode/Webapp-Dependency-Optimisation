1d58abd4b2e9b99d84fda009355e8464e73ea4af
ishadhabalia <isha.dhabalia@somaiya.edu>
2023-04-09 13:41:53 +0530

    Added levels, shapes and border legend

--- wizzcode-app/src/components/pages/Dependency.js
+++ wizzcode-app/src/components/pages/Dependency.js
@@ -1,82 +1,96 @@
 import React from "react";
-import { useState, useEffect, useContext } from 'react'
-import { Row, Col } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Accordion from 'react-bootstrap/Accordion';
-//import { Digraph } from 'react-digraph';
 import Graph from 'react-graph-vis';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 function Dependency() { 
-  const names = ["a","b","c","d","e","f"];
-  const types = [1,2,1,1,3,7];
-  const nodeTypes = {
+  const matrix = [
+    [0, 0, 1,1],
+    [1, 0, 1,0],
+    [1, 1, 0,1],
+    [1, 1, 0,0],
+  ];
+  
+  //Level 1 denotes highest level
+  const levels =[1,2,3,3];
+  const access = [1,2,1,3];
+  const type = ["normal","overriding","overriding","normal"];
+  const thread = ["no","yes","no","yes"];
+
+  const colorscheme = {
     1: '#FF0000', // red
     2: '#00FF00', // green
     3: '#0000FF',
     4:'#FFFF00',
-    5:'#800080',
-    6:'#FFA500',
-    7:'#FFC0CB',
-    8: '#40E0D0',
-    9: '#E6E6FA',
-    10: '#FF00FF'// blue
-    // blue
   };
-  
-  const links = [[true,false,true],
-  [false, false, true],
-  [true,false,false]];
-  const nodesGraph =[];
-  const edgesGraph =[];
-  for (let i = 0; i<names.length; i++) {
-    nodesGraph.push({id: i, label: `${names[i]}`, color: nodeTypes[types[i]]}); // id is just the integer which will be used to
-    // identify the nodes and will not be displayed on the graph
-  }
-  for(let i=0;i<links.length;i++)
-  {
-  for(let j=0; j<links[i].length;j++)
-  {
-  if(links[i][j]==true)
-  {
-  edgesGraph.push({from: i, to: j});
-  }
-  }
-  }
 
-    const [graph, setGraph] = useState({
-      nodes: nodesGraph,
-      edges: edgesGraph
-    });
-  
-    const options = {
-      layout: {
-        hierarchical: false
-      },
-      edges: {
-        color: 'white'
-      }
-     
+  const shapescheme = {
+    "normal" : "circle",
+    "overriding": "box"
+  };
+
+  const borderwidthscheme ={
+    "yes":5,
+    "no":0
+  };
+
+  const nodes = matrix.map((row, index) => {
+    return {
+      id: index + 1,
+      label: `Node ${index + 1}`,
+      value: 10,
+      level: levels[index],
+      borderColor: '#000000',
+      borderWidth: borderwidthscheme [thread[index]],
+      color : colorscheme[access[index]],
+      shape : shapescheme[type[index]],
+      
     };
+  });
   
-    const events = {
-      select: function(event) {
-        var { nodes, edges } = event;
-        console.log('Selected nodes:');
-        console.log(nodes);
-        console.log('Selected edges:');
-        console.log(edges);
-      }
-    };
+  const edges = [];
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (matrix[i][j] === 1) {
+      edges.push({
+        to: i + 1,
+        from: j + 1,
+      });
+    }
+  }
+}
+
+const graph = {
+  nodes: nodes,
+  edges: edges,
+};
+
+const options = {
+  layout: {
+    hierarchical: {
+      direction: 'UD',
+      sortMethod: 'directed',
+      levelSeparation: 150,
+      nodeSpacing: 350,
+      avoidOverlap: true,
+    },
+  },
+  edges: {
+    color: 'white',
+  },
+  height: '500px',
+ 
+};
+
+
+return (
+  <Graph
+    graph={graph}
+    options={options}
+    
+  />
+);
   
-    return (
-      <Graph
-        graph={graph}
-        options={options}
-        events={events}
-        style={{ height: '400px' }}
-      />
-    );
   }
   
 export default Dependency;
