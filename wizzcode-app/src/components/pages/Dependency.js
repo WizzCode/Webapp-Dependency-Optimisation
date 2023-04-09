43f26c85f2b6aa5ac6a3ab20ed2b511416e15c84
mahidedhia <mahi.dedhia@somaiya.edu>
2023-04-09 15:38:05 +0530

    dynamic data input for graph

--- wizzcode-app/src/components/pages/Dependency.js
+++ wizzcode-app/src/components/pages/Dependency.js
@@ -4,93 +4,133 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function Dependency() { 
-  const matrix = [
-    [0, 0, 1,1],
-    [1, 0, 1,0],
-    [1, 1, 0,1],
-    [1, 1, 0,0],
-  ];
-  
-  //Level 1 denotes highest level
-  const levels =[1,2,3,3];
-  const access = [1,2,1,3];
-  const type = ["normal","overriding","overriding","normal"];
-  const thread = ["no","yes","no","yes"];
-
-  const colorscheme = {
-    1: '#FF0000', // red
-    2: '#00FF00', // green
-    3: '#0000FF',
-    4:'#FFFF00',
-  };
-
-  const shapescheme = {
-    "normal" : "circle",
-    "overriding": "box"
-  };
-
-  const borderwidthscheme ={
-    "yes":5,
-    "no":0
-  };
-
-  const nodes = matrix.map((row, index) => {
-    return {
-      id: index + 1,
-      label: `Node ${index + 1}`,
-      value: 10,
-      level: levels[index],
-      borderColor: '#000000',
-      borderWidth: borderwidthscheme [thread[index]],
-      color : colorscheme[access[index]],
-      shape : shapescheme[type[index]],
-      
-    };
-  });
-  
-  const edges = [];
-for (let i = 0; i < matrix.length; i++) {
-  for (let j = 0; j < matrix[i].length; j++) {
-    if (matrix[i][j] === 1) {
-      edges.push({
-        to: i + 1,
-        from: j + 1,
-      });
+  const dependencyInfoJson = {
+    "primaryTypes": [
+      "class",
+      "classVariable",
+      "privateMethod",
+      "publicMethod",
+      "protectedMethod",
+      "defaultMethod",
+      "variable",
+    ],
+    "nodesInfo": {
+      0:{
+        "name":"SampleProgram",
+        "level":1,
+        "primaryType":"class",
+        "overriding":"y",
+        "thread":"n",
+        "depArray":[0,1,1,1],
+      },
+      1:{
+        "name":"A.f1()",
+        "level":2,
+        "primaryType":"publicMethod",
+        "overriding":"n",
+        "thread":"y",
+        "depArray":[0,0,1,0],
+      },
+      2:{
+        "name":"A.f1().abc",
+        "level":3,
+        "primaryType":"variable",
+        "overriding":"n",
+        "thread":"n",
+        "depArray":[0,0,0,0],
+      },
+      3:{
+        "name":"A.f2()",
+        "level":2,
+        "primaryType":"publicMethod",
+        "overriding":"y",
+        "thread":"y",
+        "depArray":[0,1,1,0],
+      },
     }
   }
-}
 
-const graph = {
-  nodes: nodes,
-  edges: edges,
-};
+  const primaryTypes = dependencyInfoJson["primaryTypes"];
+  const nodesInfo = dependencyInfoJson["nodesInfo"];
+  const noNodes = Object.keys(nodesInfo).length;
 
-const options = {
-  layout: {
-    hierarchical: {
-      direction: 'UD',
-      sortMethod: 'directed',
-      levelSeparation: 150,
-      nodeSpacing: 350,
-      avoidOverlap: true,
-    },
-  },
-  edges: {
-    color: 'white',
-  },
-  height: '500px',
- 
-};
-
-
-return (
-  <Graph
-    graph={graph}
-    options={options}
-    
-  />
-);
-  
+  const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#800080", "#FFA500", "#FFC0CB", "#40E0D0", "#E6E6FA", "#FF00FF"];
+  const colorsForTypes = {};
+  for(let i=0;i<primaryTypes.length;i++){
+    colorsForTypes[primaryTypes[i]]=colors[i];
   }
+
+  const graphEdges = [];
+  for( let key in nodesInfo){
+    for (let i=0;i<noNodes;i++){
+      if(nodesInfo[key]["depArray"][i] === 1){
+        graphEdges.push({
+          to: i + 1,
+          from: parseInt(key) + 1,
+        });
+      }
+    }
+  }
+
+  const shapeScheme = (item) => {
+    if(item["overriding"]==="y"){
+      return "box"
+    }
+    else{
+      return "circle"
+    }
+  }
+
+  const borderwidthscheme ={
+    "y":5,
+    "n":0
+  };
+
+  const graphNodes = Object.entries(nodesInfo).map(([key, item]) => {
+    return {
+      id: parseInt(key) + 1,
+      label: item["name"],
+      value: 10,
+      level: item["level"],
+      borderColor: '#000000',
+      borderWidth: borderwidthscheme[item["thread"]],
+      color : {
+        background:colorsForTypes[item["primaryType"]],
+      },
+      shape : shapeScheme(item),   
+    };
+  });
+
+  const graph = {
+    nodes: graphNodes,
+    edges: graphEdges,
+  };
+
+  const options = {
+    layout: {
+      hierarchical: {
+        direction: 'UD',
+        sortMethod: 'directed',
+        levelSeparation: 150,
+        nodeSpacing: 350,
+        avoidOverlap: true,
+      },
+    },
+    edges: {
+      color: 'white',
+    },
+    height:'500px',
+    autoResize: true,
+  };
+
+  return (
+    <div id="graph-div" className="rounded-4">
+      <Graph
+      graph={graph}
+      options={options}
+    />
+    </div>
+  );
+}
   
 export default Dependency;
